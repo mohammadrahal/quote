c4b750611fa5d873e33220d672e31b61e1f84ef5
Mohammad Rahal <rahalmohamad21@gmail.com>
2024-01-13 10:55:34 +0200

    fixed some errors

--- js/apiApp.js
+++ js/apiApp.js
@@ -1,24 +1,18 @@
-const quot = document.getElementById("quote");
+const quote = document.getElementById("quote");
 const title = document.querySelector("#id");
-const auth= document.querySelector("#author");
+const auth = document.querySelector("#author");
 
-
-function quote() {
-  fetch("https://api.quotable.io/random")
-  
-    .then((response =>  response.json()))
-    
-    .then (data =>{
-      // console.log(data)
-quot.textContent = '"' + data.content + '"';
-title.textContent = "advice#" + data.length ;
-auth.textContent= "author:    "  + data.author;
-// console.log(data)
-    })
-  
-    .catch(function (err) {
-      console.log(err);
-    })
+const fetchquote = () => {
+    fetch("https://api.quotable.io/random")
+        .then(response => response.json())
+        .then(data => {
+            quote.textContent = '"' + data.content + '"';
+            title.textContent = "Quote#" + data.length;
+            auth.textContent = "author:    " + data.author;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
-//  Repeat QuoteGenerate() every 10 seconds
-setInterval(quote(), 10000)
+
+setInterval(fetchquote(), 10000); 
